a98ba93eeb26f4a077c94b0aa398b7fee808cac7
MWisent <towisent@gmail.com>
2024-04-06 20:09:18 +0200

    initial commit

---
+++ src/main/java/com/vesqum/Resourcemanagment/ResourcemanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.vesqum.Resourcemanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcemanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcemanagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vesqum/Resourcemanagment/ResourcemanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vesqum.Resourcemanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcemanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
